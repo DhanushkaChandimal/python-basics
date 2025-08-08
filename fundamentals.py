9c083e6324ef0f822033b82b6c7832e4a30d3796
Dhanushka <dhanushkamcr@gmail.com>
2025-08-08 17:49:22 -0500

    Python integers

--- fundamentals.py
+++ fundamentals.py
@@ -6,6 +6,8 @@ import random
 # print(age)
 # print(color)
 
+
+
 # score = int(input("Score: "))
 # if score>90:
 #     print("Excellent")
@@ -14,50 +16,83 @@ import random
 # else:
 #     print("Needs Improvement")
 
-first_name = "Dhanushka"
-last_name = "Chandimal"
-full_name = first_name + " " + last_name
-multi_line = """This is first line 
-This is second line"""
-print(full_name)
-print("length of name = " + str(len(full_name)))
-print(full_name[0])
-print(multi_line)
-print("wow! " * 3)
-print(full_name[1:4])
-print(full_name[3:])
-print(full_name[:5])
-print(full_name[-1])
-print(full_name[-3])
-print(full_name.upper())
-print(full_name.lower())
 
-string = "            test           "
-print(">>>" + string + "<<<")
-print(">>>" + string.strip() + "<<<")
 
-string = "This is original text.pdf"
-print(string)
-print(string.replace("original", "replaced"))
-print(string.split())
-print(string.split("a"))
+# first_name = "Dhanushka"
+# last_name = "Chandimal"
+# full_name = first_name + " " + last_name
+# multi_line = """This is first line 
+# This is second line"""
+# print(full_name)
+# print("length of name = " + str(len(full_name)))
+# print(full_name[0])
+# print(multi_line)
+# print("wow! " * 3)
+# print(full_name[1:4])
+# print(full_name[3:])
+# print(full_name[:5])
+# print(full_name[-1])
+# print(full_name[-3])
+# print(full_name.upper())
+# print(full_name.lower())
 
-list1 = ["1", "23", "asdsa"]
-print(" ".join(list1))
+# string = "            test           "
+# print(">>>" + string + "<<<")
+# print(">>>" + string.strip() + "<<<")
 
-print(string.endswith(".pdf"))
-print(string.endswith(".gif"))
-print(string.endswith("f"))
+# string = "This is original text.pdf"
+# print(string)
+# print(string.replace("original", "replaced"))
+# print(string.split())
+# print(string.split("a"))
 
-age = 30
+# list1 = ["1", "23", "asdsa"]
+# print(" ".join(list1))
 
-print("My name is {} and I am {} years old.".format(first_name, age))
-print("My name is {}.".format(first_name, age))
-# print("My name is {} and I am {} years old.".format(first_name))
-print(f"My name is {first_name} and I am {age} years old.")
+# print(string.endswith(".pdf"))
+# print(string.endswith(".gif"))
+# print(string.endswith("f"))
 
-first_names = ["Christian", "Dylan", "Travis", "Katelyn"]
-last_names = ["Sachs", "Katina", "Peck", "Mehner"]
+# age = 30
 
-print("Random name: " + first_names[random.randint(0,len(first_names)-1)] + " " + last_names[random.randint(0,len(last_names)-1)])
-print("Random name: " + random.choice(first_names) + " " + random.choice(last_names))
+# print("My name is {} and I am {} years old.".format(first_name, age))
+# print("My name is {}.".format(first_name, age))
+# # print("My name is {} and I am {} years old.".format(first_name))
+# print(f"My name is {first_name} and I am {age} years old.")
+
+# first_names = ["Christian", "Dylan", "Travis", "Katelyn"]
+# last_names = ["Sachs", "Katina", "Peck", "Mehner"]
+
+# print("Random name: " + first_names[random.randint(0,len(first_names)-1)] + " " + last_names[random.randint(0,len(last_names)-1)])
+# print("Random name: " + random.choice(first_names) + " " + random.choice(last_names))
+
+
+
+x = 20
+y = 3
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+print(x % y)
+print(x ** y)
+print(pow(x,y))
+print(abs(-15))
+print(round(-15.2))
+print(round(-15.6))
+print(round(15.2))
+print(round(15.6))
+print(int("256") + 1)
+
+print(7 % 3)
+print(-7 % 3)
+print(7 % -3)
+print(-7 % -3)
+
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+print(f"{num1} + {num2} = {num1 + num2}")
+print(f"{num1} - {num2} = {num1 - num2}")
+print(f"{num1} * {num2} = {num1 * num2}")
+print(f"{num1} / {num2} = {num1 / num2}")
