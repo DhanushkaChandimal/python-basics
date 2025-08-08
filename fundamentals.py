5b55fc2042240493677130e068d216bf2d95016f
Dhanushka <dhanushkamcr@gmail.com>
2025-08-08 16:57:37 -0500

    Control structure

--- fundamentals.py
+++ fundamentals.py
@@ -1,7 +1,14 @@
-name = input("What is your name? ")
-age = input("What is your age? ")
-color = input("What is your facourite color? ")
-print(name)
-print(age)
-print(color)
+# name = input("What is your name? ")
+# age = input("What is your age? ")
+# color = input("What is your facourite color? ")
+# print(name)
+# print(age)
+# print(color)
 
+score = int(input("Score: "))
+if score>90:
+    print("Excellent")
+elif score>70:
+    print("Good")
+else:
+    print("Needs Improvement")
