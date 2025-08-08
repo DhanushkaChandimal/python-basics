fd966a0677a975bbc25a7911b87829c9d3e0191c
Dhanushka <dhanushkamcr@gmail.com>
2025-08-08 17:28:27 -0500

    Strings in python

--- fundamentals.py
+++ fundamentals.py
@@ -1,3 +1,4 @@
+import random
 # name = input("What is your name? ")
 # age = input("What is your age? ")
 # color = input("What is your facourite color? ")
@@ -5,10 +6,58 @@
 # print(age)
 # print(color)
 
-score = int(input("Score: "))
-if score>90:
-    print("Excellent")
-elif score>70:
-    print("Good")
-else:
-    print("Needs Improvement")
+# score = int(input("Score: "))
+# if score>90:
+#     print("Excellent")
+# elif score>70:
+#     print("Good")
+# else:
+#     print("Needs Improvement")
+
+first_name = "Dhanushka"
+last_name = "Chandimal"
+full_name = first_name + " " + last_name
+multi_line = """This is first line 
+This is second line"""
+print(full_name)
+print("length of name = " + str(len(full_name)))
+print(full_name[0])
+print(multi_line)
+print("wow! " * 3)
+print(full_name[1:4])
+print(full_name[3:])
+print(full_name[:5])
+print(full_name[-1])
+print(full_name[-3])
+print(full_name.upper())
+print(full_name.lower())
+
+string = "            test           "
+print(">>>" + string + "<<<")
+print(">>>" + string.strip() + "<<<")
+
+string = "This is original text.pdf"
+print(string)
+print(string.replace("original", "replaced"))
+print(string.split())
+print(string.split("a"))
+
+list1 = ["1", "23", "asdsa"]
+print(" ".join(list1))
+
+print(string.endswith(".pdf"))
+print(string.endswith(".gif"))
+print(string.endswith("f"))
+
+age = 30
+
+print("My name is {} and I am {} years old.".format(first_name, age))
+print("My name is {}.".format(first_name, age))
+# print("My name is {} and I am {} years old.".format(first_name))
+print(f"My name is {first_name} and I am {age} years old.")
+
+first_names = ["Christian", "Dylan", "Travis", "Katelyn"]
+last_names = ["Sachs", "Katina", "Peck", "Mehner"]
+
+print("Random name: " + first_names[random.randint(0,len(first_names)-1)] + " " + last_names[random.randint(0,len(last_names)-1)])
+print("Random name: " + random.choice(first_names) + " " + random.choice(last_names))
