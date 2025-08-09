1cd1f0793fd7a042eb4b03c21d6ffeea47be9116
Dhanushka <dhanushkamcr@gmail.com>
2025-08-08 19:26:13 -0500

    Lists in python

--- fundamentals.py
+++ fundamentals.py
@@ -107,13 +107,87 @@ import random
 # else:
 #     print("Relax indoors.")
 
-random_number = random.randint(1,10)
-user_input = int(input())
-if(random_number > user_input):
-    print("too low")
-    user_input = int(input())
-elif(random_number < user_input):
-    print("too high")
-    user_input = int(input())
-else:
-    print("Congratulations you guessed corectly")
+# random_number = random.randint(1,10)
+# user_input = int(input())
+# if(random_number > user_input):
+#     print("too low")
+#     user_input = int(input())
+# elif(random_number < user_input):
+#     print("too high")
+#     user_input = int(input())
+# else:
+#     print("Congratulations you guessed corectly")
+
+
+
+my_list = [1, 2.34, "Srtring", True, 3, 4, 5, 6]
+
+print(my_list[0])
+print(my_list[1])
+print(my_list[-1])
+print(my_list[-2])
+print(my_list[-3])
+
+my_list.append("del")
+print(my_list)
+
+my_list.insert(2,"del")
+print(my_list)
+
+my_list.remove("del") # remove only first matching item
+print(my_list)
+
+del my_list[0]
+print(my_list)
+
+print(my_list.pop(1))
+print(my_list)
+
+print(my_list[1:5])
+print(my_list[:3])
+print(my_list[3:])
+print(my_list[3:3])
+
+fruits = ["apple", "banana", "cherry", "date"]
+print(fruits[0])
+print(fruits[-1])
+fruits.append("elderberry")
+print(fruits)
+fruits.insert(1, "blueberry")
+print(fruits)
+fruits.remove("banana")
+print(fruits)
+del fruits[0]
+print(fruits)
+citrus_fruits = fruits[1:3]
+print(citrus_fruits)
+
+print(len(fruits))
+print(min(fruits))
+print(max(fruits))
+
+numbers = [55, 1, 5, 3, -50, -2]
+print(numbers)
+print(len(numbers))
+print(min(numbers))
+print(max(numbers))
+numbers.sort()
+print(numbers)
+numbers.reverse()
+print(numbers)
+print(numbers + fruits)
+numbers.extend(fruits)
+print(numbers)
+
+nested_list = [[0, 1, 2], [11, 12, 13], [21, 22, 23]]
+print(nested_list)
+print(nested_list[1])
+print(nested_list[1][2])
+
+favourites_books = []
+favourites_books.append(input("Enter your first favourite book: "))
+favourites_books.append(input("Enter your second favourite book: "))
+favourites_books.append(input("Enter your third favourite book: "))
+print(favourites_books)
+favourites_books.sort()
+print(favourites_books)
